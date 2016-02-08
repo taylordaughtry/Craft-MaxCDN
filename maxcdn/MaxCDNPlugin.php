5f9e9ac453dedfefe92495612779514b512d6257
Taylor Daughtry <hello@taylordaughtry.com>
2016-02-07 21:52:14 -0600

    Fix Sublime's addition of spaces instead of tabs

--- maxcdn/MaxCDNPlugin.php
+++ maxcdn/MaxCDNPlugin.php
@@ -3,172 +3,172 @@ namespace Craft;
 
 class MaxCDNPlugin extends BasePlugin
 {
-    /**
-     * If your plugin name is a proper noun, you shouldn't allow it to be
-     * translated. However, if it's a somewhat generic name, it's a good
-     * idea to wrap the string in Craft::t('JumpStart').
-     *
-     * @return string
-     */
-    public function getName()
-    {
-         return 'MaxCDN';
-    }
+	/**
+	 * If your plugin name is a proper noun, you shouldn't allow it to be
+	 * translated. However, if it's a somewhat generic name, it's a good
+	 * idea to wrap the string in Craft::t('JumpStart').
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		 return 'MaxCDN';
+	}
 
-    /**
-     * Added in 2.5, this text displays on the Plugins page.
-     *
-     * @return string
-     */
-    public function getDescription()
-    {
-        return 'View stats and purge MaxCDN files right from the Craft CP.';
-    }
+	/**
+	 * Added in 2.5, this text displays on the Plugins page.
+	 *
+	 * @return string
+	 */
+	public function getDescription()
+	{
+		return 'View stats and purge MaxCDN files right from the Craft CP.';
+	}
 
-    /**
-     * This is displayed in the Plugin listing page. Incrementing this does
-     * NOT take the site down for updates, only getSchemaVersion() does that.
-     *
-     * @return string
-     */
-    public function getVersion()
-    {
-        return '0.1.0';
-    }
+	/**
+	 * This is displayed in the Plugin listing page. Incrementing this does
+	 * NOT take the site down for updates, only getSchemaVersion() does that.
+	 *
+	 * @return string
+	 */
+	public function getVersion()
+	{
+		return '0.1.0';
+	}
 
-    /**
-     * Increment this if you have new migrations that needto be run.
-     * Craft only takes the site down for updates when you
-     * increment this method.
-     *
-     * @return string
-     */
-    public function getSchemaVersion()
-    {
-        return '1.1.0';
-    }
+	/**
+	 * Increment this if you have new migrations that needto be run.
+	 * Craft only takes the site down for updates when you
+	 * increment this method.
+	 *
+	 * @return string
+	 */
+	public function getSchemaVersion()
+	{
+		return '1.1.0';
+	}
 
-    /**
-     * Self explanatory.
-     *
-     * @return string
-     */
-    public function getDeveloper()
-    {
-        return 'Taylor Daughtry';
-    }
+	/**
+	 * Self explanatory.
+	 *
+	 * @return string
+	 */
+	public function getDeveloper()
+	{
+		return 'Taylor Daughtry';
+	}
 
-    /**
-     * Self explanatory.
-     *
-     * @return string
-     */
-    public function getDeveloperUrl()
-    {
-        return 'https://github.com/taylordaughtry';
-    }
+	/**
+	 * Self explanatory.
+	 *
+	 * @return string
+	 */
+	public function getDeveloperUrl()
+	{
+		return 'https://github.com/taylordaughtry';
+	}
 
-    /**
-     * Creates a link to your docs on the Plugins page.
-     *
-     * @return string
-     */
-    public function getDocumentationUrl()
-    {
-        return 'https://github.com/taylordaughtry/Craft-MaxCDN';
-    }
+	/**
+	 * Creates a link to your docs on the Plugins page.
+	 *
+	 * @return string
+	 */
+	public function getDocumentationUrl()
+	{
+		return 'https://github.com/taylordaughtry/Craft-MaxCDN';
+	}
 
-    /**
-     * Added in 2.5, plugins can now take part in Craft's update notifications,
-     * and can include their own changelog on the Updates page.
-     *
-     * @return string
-     */
-    public function getReleaseFeedUrl()
-    {
-        return 'https://raw.githubusercontent.com/' .
-        'taylordaughtry/craft-maxcdn/master/releases.json';
-    }
+	/**
+	 * Added in 2.5, plugins can now take part in Craft's update notifications,
+	 * and can include their own changelog on the Updates page.
+	 *
+	 * @return string
+	 */
+	public function getReleaseFeedUrl()
+	{
+		return 'https://raw.githubusercontent.com/' .
+		'taylordaughtry/craft-maxcdn/master/releases.json';
+	}
 
-    /**
-     * Your icon is displayed on the Plugins page, as well as the Settings page
-     * IF your plugin has its own settings.
-     *
-     * @return string
-     */
-    public function getIconPath()
-    {
-        return craft()->path->getPluginsPath().'maxcdn/resources/icon.svg';
-    }
+	/**
+	 * Your icon is displayed on the Plugins page, as well as the Settings page
+	 * IF your plugin has its own settings.
+	 *
+	 * @return string
+	 */
+	public function getIconPath()
+	{
+		return craft()->path->getPluginsPath().'maxcdn/resources/icon.svg';
+	}
 
-    /**
-     * Generates the HTML for your settings. This should almost always return a
-     * template that renders from a twig file.
-     *
-     * @return string
-     */
-    public function getSettingsHtml()
-    {
-        return craft()->templates->render('maxcdn/settings', [
-            'settings' => $this->getSettings()
-        ]);
-    }
+	/**
+	 * Generates the HTML for your settings. This should almost always return a
+	 * template that renders from a twig file.
+	 *
+	 * @return string
+	 */
+	public function getSettingsHtml()
+	{
+		return craft()->templates->render('maxcdn/settings', [
+			'settings' => $this->getSettings()
+		]);
+	}
 
-    /**
-     * Declare the settings for your plugin here. Get these values by calling
-     * $this->getSettings() anywhere in THIS file. If you're trying
-     * to do this in any other file, you'll have to go through the craft
-     * plugins service, like so:
-     *
-     *  craft()->plugins->getPlugin('jumpstart')->getSettings();
-     *
-     * @return array
-     */
-    protected function defineSettings()
-    {
-        return [
-            'sample_setting' => array(AttributeType::Mixed, 'default' => ''),
-            'alias' => array(AttributeType::String, 'default' => ''),
-            'consumerKey' => array(AttributeType::String, 'default' => ''),
-            'consumerSecret' => array(AttributeType::String, 'default' => ''),
-        ];
-    }
+	/**
+	 * Declare the settings for your plugin here. Get these values by calling
+	 * $this->getSettings() anywhere in THIS file. If you're trying
+	 * to do this in any other file, you'll have to go through the craft
+	 * plugins service, like so:
+	 *
+	 *  craft()->plugins->getPlugin('jumpstart')->getSettings();
+	 *
+	 * @return array
+	 */
+	protected function defineSettings()
+	{
+		return [
+			'sample_setting' => array(AttributeType::Mixed, 'default' => ''),
+			'alias' => array(AttributeType::String, 'default' => ''),
+			'consumerKey' => array(AttributeType::String, 'default' => ''),
+			'consumerSecret' => array(AttributeType::String, 'default' => ''),
+		];
+	}
 
-    /**
-     * If your plugin needs its own item in the Navigation, set this to true. Be
-     * Sure and update the icon-mask.svg file; it's the icon that shows next to
-     * your plugin name in the CP Navigation.
-     *
-     * @return boolean
-     */
-    public function hasCpSection()
-    {
-        return true;
-    }
+	/**
+	 * If your plugin needs its own item in the Navigation, set this to true. Be
+	 * Sure and update the icon-mask.svg file; it's the icon that shows next to
+	 * your plugin name in the CP Navigation.
+	 *
+	 * @return boolean
+	 */
+	public function hasCpSection()
+	{
+		return true;
+	}
 
-    /**
-     * Make vendors available within your plugin.
-     *
-     * @return string
-     */
-    public function init()
-    {
-        require CRAFT_PLUGINS_PATH.'/maxcdn/vendor/autoload.php';
-    }
+	/**
+	 * Make vendors available within your plugin.
+	 *
+	 * @return string
+	 */
+	public function init()
+	{
+		require CRAFT_PLUGINS_PATH.'/maxcdn/vendor/autoload.php';
+	}
 
-    /**
-     * Tell Craft where to send our plugin's URLs.
-     *
-     * @method registerCpRoutes
-     *
-     * @return array
-     */
-    public function registerCpRoutes()
-    {
-        return [
-            'maxcdn' => ['action' => 'MaxCDN/index'],
-            'maxcdn/zones' => ['action' => 'MaxCDN/zones'],
-            'maxcdn/cache' => ['action' => 'MaxCDN/cache'],
-        ];
-    }
+	/**
+	 * Tell Craft where to send our plugin's URLs.
+	 *
+	 * @method registerCpRoutes
+	 *
+	 * @return array
+	 */
+	public function registerCpRoutes()
+	{
+		return [
+			'maxcdn' => ['action' => 'MaxCDN/index'],
+			'maxcdn/zones' => ['action' => 'MaxCDN/zones'],
+			'maxcdn/cache' => ['action' => 'MaxCDN/cache'],
+		];
+	}
 }
--- maxcdn/services/MaxCDNService.php
+++ maxcdn/services/MaxCDNService.php
@@ -3,137 +3,137 @@ namespace Craft;
 
 class MaxCDNService extends BaseApplicationComponent
 {
-    /*
-        Plugin Settings
-     */
-    protected $settings;
+	/*
+		Plugin Settings
+	 */
+	protected $settings;
 
-    /*
-        An instance of the MaxCDN API.
-     */
+	/*
+		An instance of the MaxCDN API.
+	 */
 	protected $api;
 
-    public function __construct()
-    {
-    	$this->settings =craft()->plugins->getPlugin('maxcdn')->getSettings();
+	public function __construct()
+	{
+		$this->settings =craft()->plugins->getPlugin('maxcdn')->getSettings();
 
-    	$this->api = new \NetDNA(
-	    	$this->settings->alias,
-	    	$this->settings->consumerKey,
-	    	$this->settings->consumerSecret
-    	);
-    }
+		$this->api = new \NetDNA(
+			$this->settings->alias,
+			$this->settings->consumerKey,
+			$this->settings->consumerSecret
+		);
+	}
 
-    /**
-     * Get a list of zones. This DOES NOT include stats, just the ID,
-     * name, and other relevant metadata about the zone.
-     *
-     * @method getZones
-     *
-     * @return array
-     */
-    public function getZones()
-    {
-        return $this->callApi('/zones.json', 'zones');
-    }
+	/**
+	 * Get a list of zones. This DOES NOT include stats, just the ID,
+	 * name, and other relevant metadata about the zone.
+	 *
+	 * @method getZones
+	 *
+	 * @return array
+	 */
+	public function getZones()
+	{
+		return $this->callApi('/zones.json', 'zones');
+	}
 
-    /**
-     * Get the stats for a provided Zone. Note that an ID will generally
-     * be something like '12345'. Don't confuse the indexes on the
-     * zones array with the zone's actual ID.
-     *
-     * @param int $id
-     * @return object
-     */
-    public function getZoneStats($id)
-    {
-        $response = $this->callApi('/reports/' . $id . '/stats.json', 'stats');
+	/**
+	 * Get the stats for a provided Zone. Note that an ID will generally
+	 * be something like '12345'. Don't confuse the indexes on the
+	 * zones array with the zone's actual ID.
+	 *
+	 * @param int $id
+	 * @return object
+	 */
+	public function getZoneStats($id)
+	{
+		$response = $this->callApi('/reports/' . $id . '/stats.json', 'stats');
 
-        // TODO: Will likely break here with multiple zones.
-        // Patch when you have multiple zones to test.
-        $response->size = $this->convertSize($response->size, 'GB');
+		// TODO: Will likely break here with multiple zones.
+		// Patch when you have multiple zones to test.
+		$response->size = $this->convertSize($response->size, 'GB');
 
-        return $response;
-    }
+		return $response;
+	}
 
-    /**
-     * Get the files in a zone, sorted by hits.
-     *
-     * @return object
-     */
-    public function getPopularFiles()
-    {
-        $response = $this->callApi('/reports/popularfiles.json', 'popularfiles');
+	/**
+	 * Get the files in a zone, sorted by hits.
+	 *
+	 * @return object
+	 */
+	public function getPopularFiles()
+	{
+		$response = $this->callApi('/reports/popularfiles.json', 'popularfiles');
 
-        foreach ($response as &$file) {
-            $file->size = $this->convertSize($file->size, 'GB');
-        }
+		foreach ($response as &$file) {
+			$file->size = $this->convertSize($file->size, 'GB');
+		}
 
-        return $response;
-    }
+		return $response;
+	}
 
-    /**
-     * Delete the zone by its provided ID.
-     *
-     * @param  int $zoneId
-     * @return void
-     */
-    public function purgeFiles($zoneId)
-    {
-        $this->callApi('/zones/pull.json/' . $zoneId . '/cache', null, 'delete');
-    }
+	/**
+	 * Delete the zone by its provided ID.
+	 *
+	 * @param  int $zoneId
+	 * @return void
+	 */
+	public function purgeFiles($zoneId)
+	{
+		$this->callApi('/zones/pull.json/' . $zoneId . '/cache', null, 'delete');
+	}
 
-    /**
-     * Helper method to convert sizes. Taken from Maxee.
-     *
-     * @param int $size The file's size in bytes
-     * @param string $unit
-     *
-     * @return string
-     */
-    public function convertSize($size, $unit = '')
-    {
-        if ((! $unit and $size >= 1<<30) or $unit == 'GB') {
-            return number_format($size / (1<<30), 2).'GB';
-        }
+	/**
+	 * Helper method to convert sizes. Taken from Maxee.
+	 *
+	 * @param int $size The file's size in bytes
+	 * @param string $unit
+	 *
+	 * @return string
+	 */
+	public function convertSize($size, $unit = '')
+	{
+		if ((! $unit and $size >= 1<<30) or $unit == 'GB') {
+			return number_format($size / (1<<30), 2).'GB';
+		}
 
-        if ((! $unit and $size >= 1<<20) or $unit == 'MB') {
-            return number_format($size / (1<<20), 2).'MB';
-        }
+		if ((! $unit and $size >= 1<<20) or $unit == 'MB') {
+			return number_format($size / (1<<20), 2).'MB';
+		}
 
-        if ((! $unit and $size >= 1<<10) or $unit == 'KB') {
-            return number_format($size / (1<<10), 2) . 'KB';
-        }
+		if ((! $unit and $size >= 1<<10) or $unit == 'KB') {
+			return number_format($size / (1<<10), 2) . 'KB';
+		}
 
-        return number_format($size) . ' bytes';
-    }
+		return number_format($size) . ' bytes';
+	}
 
-    /**
-     * Make a call to the MaxCDN API with the provided endpoint, then
-     * decode the response and return the data itself.
-     *
-     * @method callApi
-     *
-     * @param string $endpoint API URL
-     *
-     * @return array
-     */
-    private function callApi($endpoint, $reportType, $callType = '')
-    {
+	/**
+	 * Make a call to the MaxCDN API with the provided endpoint, then
+	 * decode the response and return the data itself.
+	 *
+	 * @method callApi
+	 *
+	 * @param string $endpoint API URL
+	 *
+	 * @return array
+	 */
+	private function callApi($endpoint, $reportType, $callType = '')
+	{
 
-        // TODO: If this is confirmed to clear the cache, do proper
-        // checking for the response code.
-        if ($callType) {
-            switch ($callType) {
-                case 'delete':
-                    $response = $this->api->delete($endpoint);
-                    return true;
-                    break;
-            }
-        }
+		// TODO: If this is confirmed to clear the cache, do proper
+		// checking for the response code.
+		if ($callType) {
+			switch ($callType) {
+				case 'delete':
+					$response = $this->api->delete($endpoint);
+					return true;
+					break;
+			}
+		}
 
-        $response = $this->api->get($endpoint);
+		$response = $this->api->get($endpoint);
 
-        return json_decode($response)->data->$reportType;
-    }
+		return json_decode($response)->data->$reportType;
+	}
 }
